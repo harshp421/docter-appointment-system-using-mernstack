03e09e3565cf7b82a1e49d10b12f482fd603da5a
harsh <harshparmar0421@gmail.com>
2024-02-02 13:11:05 +0530

    add deployee setting

--- config/db.js
+++ config/db.js
@@ -5,6 +5,7 @@ dotenv.config();
 mongoose.set('strictQuery', true);
 const connectDB = async () => {
   try {
+    console.log(process.env.MONGO_CONNECT,"connect string")
     await mongoose.connect(process.env.MONGO_CONNECT);
     console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
   } catch (error) {
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const colors = require("colors");
 const moragan = require("morgan");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const cors=require("cors");
 
 //dotenv conig
 dotenv.config();
@@ -16,7 +17,7 @@ const app = express();
 //middlewares
 app.use(express.json());
 app.use(moragan("dev"));
-
+app.use(cors())
 //routes
 app.use("/api/v1/user", require("./routes/userRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
