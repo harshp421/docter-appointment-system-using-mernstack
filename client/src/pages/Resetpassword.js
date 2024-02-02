000b2be2f2a38a6e0f42917b9b3d9bd4554cb03b
harsh <harshparmar0421@gmail.com>
2024-02-02 14:09:03 +0530

    #

--- client/src/pages/Resetpassword.js
+++ client/src/pages/Resetpassword.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Container from '../Component/Container'
 import { useNavigate, useParams } from 'react-router-dom'
-import { set } from 'mongoose';
+
 const Resetpassword = () => {
 
     var id;
